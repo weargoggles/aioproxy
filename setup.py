4b8c47c68413597867af3e15bbb644f3d7b4a9b4
Pete Wildsmith <pete@dabapps.com>
2015-11-08 19:08:05 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='aioproxy',
+    version='0.0.1',
+    description='reverse proxy with aiohttp',
+    author='Pete Wildsmith',
+    author_email='pete@weargoggles.co.uk',
+    url='https://github.com/weargoggles/aioproxy',
+    py_modules=['aioproxy'],
+)

030cec654b0ee3c4f10c5d79db70b80620c3a271
Pete Wildsmith <pete@dabapps.com>
2015-11-09 11:20:17 +0000

    proxy rocksy

--- aioproxy.py
+++ aioproxy.py
@@ -7,14 +7,30 @@ import aiohttp.client
 import aiohttp.abc
 
 
-THE_HOST = 'google.com'
-
-
 async def the_handler(request):
-    return aiohttp.web.Response(body=b"<h1>Hello, asyncio</h1>")
+    host, port = request.host.split(':')
+    if host == 'localhost':
+        host = 'www.weargoggles.co.uk'
+    async with aiohttp.client.request(
+        request.method,
+        'http://' + host + request.path,
+        chunked=1024
+    ) as r:
+        response = aiohttp.web.StreamResponse(status=r.status,
+                                              headers=r.headers)
+        await response.prepare(request)
+        content = r.content
+        while True:
+            chunk = await content.read(1024)
+            if not chunk:
+                break
+            response.write(chunk)
+        await response.write_eof()
+        return response
 
 
 class ReverseProxyMatch(aiohttp.abc.AbstractMatchInfo):
+    # noinspection PyMethodOverriding
     def handler(self, request):
         return the_handler(request)
 
@@ -30,28 +46,13 @@ class AllRouter(aiohttp.abc.AbstractRouter):
         return ReverseProxyMatch()
 
 
-class ReverseProxyHandler(aiohttp.server.ServerHttpProtocol):
-
-    async def handle_request(self, message, payload):
-        url = 'http://' + THE_HOST + message.path
-        print(url)
-        async with aiohttp.client.request(message.method, url) as r:
-            response = aiohttp.Response(self.writer, r.status,
-                                        http_version=message.version)
-            print({i: r.headers[i] for i in r.headers})
-            response.add_headers(*[(i, r.headers[i]) for i in r.headers])
-            response.send_headers()
-            response.write(r.content.read())
-            await response.write_eof()
-
-
 if __name__ == "__main__":
     import asyncio
+
     loop = asyncio.get_event_loop()
     app = aiohttp.web.Application(router=AllRouter())
     handler = app.make_handler()
     f = loop.create_server(handler, '0.0.0.0', '8088')
-    # f = loop.create_server(lambda: ReverseProxyHandler(debug=True, keep_alive=75), '0.0.0.0', '8088')
     srv = loop.run_until_complete(f)
     print("serving on:", srv.sockets[0].getsockname())
     try:

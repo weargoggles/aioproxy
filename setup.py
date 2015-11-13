5ed7fcc6b0d1fbd377b5cc23a3e0aa872d0b4857
Pete Wildsmith <pete@dabapps.com>
2015-11-13 14:56:33 +0000

    0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='aioproxy',
-    version='0.0.1',
+    version='0.0.2',
     description='reverse proxy with aiohttp',
     author='Pete Wildsmith',
     author_email='pete@weargoggles.co.uk',
